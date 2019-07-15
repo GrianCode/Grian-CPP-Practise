d8c0a8881e32b21060c5240b722c265c3086d799
Grian <gst.grian@outlook.com>
2019-07-15 18:29:09 +0800

    pure

---
+++ CPP Files/Chap. III/3.4/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int mark;
+	cin>>mark;
+	string a="非常";
+	string b="欢迎您参加音乐社";
+	if(mark>80)
+	{
+		cout<<a<<b;	
+	}
+	else
+	{
+		cout<<b; 
+	return 0;
+}
+}
