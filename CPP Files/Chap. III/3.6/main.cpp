26ea108347a41162b8dc52cd708664b3d28541a9
Grian <gst.grian@outlook.com>
2019-07-15 19:21:53 +0800

    ：-）

---
+++ CPP Files/Chap. III/3.6/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+ 	
+	int many,a,b;
+	cin>>many;
+	a=many/8;
+	b=(many-a)*3;
+	string out="商城";
+	
+	if((many*3*0.88)>b)
+	{
+		cout<<"大洋"<<out;
+	}
+	else
+	{
+		cout<<"百汇"<<out;
+	}
+	return 0;
+}
+
