e225e1e12e2b8b396e672ccc62ff5d5212bc6456
Grian <52743201+GrianCode@users.noreply.github.com>
2019-09-01 20:03:25 +0800

    Update PQ.cpp

--- PQ.cpp
+++ PQ.cpp
@@ -1,8 +1,13 @@
+/*
+	Name:PQ.cpp
+	author:Grian
+*/
 #include<iostream>
 using namespace std;
 int main() {
 	int x0 = 3, y0 = 60, p = 0, q = 0;
 	int a = 0, b = 0, c = 0;
+	int ecx = 0;
 	for (p = x0; p <= y0; p+=x0) {
 		if (y0 % p == 0) {
 			for (q = x0; q <= y0; q+=x0) {
@@ -17,26 +22,24 @@ int main() {
 							break;
 						}
 					}
-				int j;
-				bool lcm = false;
-				for (j = p; j <= y0; j += j) {		//最小公倍数 
-					if (lcm){
-						break;
-					} 
-					for (int k = q; k <= y0; k += k) {
-						if (j == k) {
-							lcm = false;
-							break;
-						}
-					}
+				int j = 0;
+				int a = 0, b = 0;
+				a = p;
+				b = q;
+				while (a % b != 0 && b != 0) {
+					int c = a % b;
+					a = b;
+					b = c;
 				}
-				if (c == x0 && j==y0) {
-					cout << p << " " << q << endl;
+				int lcm = p * q / b;
+				if (c == x0 && lcm==y0) {
+					//cout<<p<<" "<<q<<endl;
+					ecx++;
 				}
-
 				}
 			}
 		}
 	}
+	cout <<ecx;
 	return 0;
 }
