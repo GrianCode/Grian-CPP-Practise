701ca38b822151a43530bd62925d57906300bf5b
Grian <52743201+GrianCode@users.noreply.github.com>
2019-09-03 22:11:13 +0800

    OMG :broken_heart:
    :-(

--- CPP Files/Chap. III/未整理/4.39.cpp
+++ CPP Files/Chap. III/未整理/4.39.cpp
@@ -2,14 +2,19 @@
 using namespace std;
 int main(){
 	int i = 0, n = 10, c = 0;
+	int m = 0; 
 	cin >> n;
 	for (i = 1; i < n; i++) {
 		for (c = 1; c < n - i; c++) {
 			cout << " ";
 		}
-		for (c = 1; c <= 2 * i - 1; c++) {
-			cout << "*";
-		}
+		for(m = 1; m <= (2 * i - 1) / 2 + 1; m++){
+			cout << m;
+					
+			for(; m >= 1; m--){
+				cout<<m;
+			}
+		} 
 		cout << endl;
 	}
 	return 666;
---
+++ CPP Files/Chap. III/未整理/TA - 环形叁角形.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+int main(){
+	for(int i=1;i<=20000;i++){
+		cout<<9;
+	}
+	return 0;
+}
