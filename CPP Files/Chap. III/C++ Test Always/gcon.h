91d37c259c082b2c013fbb725cc3a606245cbc61
Grian <52743201+GrianCode@users.noreply.github.com>
2019-09-22 10:38:17 +0800

    still update :anguished:

--- CPP Files/Chap. III/C++ Test Always/gcon.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include<iostream>
-#include<string>
-void title(string str,str) {
-	system("("title "+str1).c_str()");
-}
--- CPP Files/Chap. III/C++ Test Always/标头.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include<iostream>
-void title(string str1, str1) {
-	system("("title "+str1).c_str");
-}
